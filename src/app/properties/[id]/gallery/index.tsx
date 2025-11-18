1071d64541f5717556b3fb53caa1b6a8ce0de2c6
Mohamed Sameh <mxsameh@gmail.com>
2025-11-18 20:09:38 +0200

    sync

--- src/app/properties/[id]/gallery/index.tsx
+++ src/app/properties/[id]/gallery/index.tsx
@@ -1,7 +1,6 @@
 "use client";
 import s from "./_s.module.css";
 
-import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import { useEffect, useState } from "react";
