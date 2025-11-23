a2a6bce0bc4c08d17b065ce5b8492c414fa66cef
Mohamed Sameh <mxsameh@gmail.com>
2025-11-23 18:14:15 +0200

    sync

--- src/app/properties/page.tsx
+++ src/app/properties/page.tsx
@@ -48,7 +48,7 @@ export default async function Properties({ searchParams }) {
           properties={properties}
         />
       </div>
-      {/* <Animation /> */}
+      <Animation />
     </>
   );
 }
