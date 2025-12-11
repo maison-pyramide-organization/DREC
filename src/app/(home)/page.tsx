d1ea84bb668e73f0bdb7c894041114ee96dcf16c
Mohamed Sameh <mxsameh@gmail.com>
2025-12-11 16:38:43 +0200

    sync

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -14,6 +14,7 @@ import getHighlights from "@/services/api/highlights";
 
 export default async function Home() {
   const highlights = await getHighlights();
+  console.log("h", highlights);
 
   return (
     <>
